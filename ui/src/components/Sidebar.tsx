f887a8e1e32e6c286b1dbdaa9b47ec482156bd97
davidchuang5 <davidchuang5@gmail.com>
2023-06-16 10:16:48 -0500

    Feat: Added time filtering to the sidebar

--- ui/src/components/Sidebar.tsx
+++ ui/src/components/Sidebar.tsx
@@ -4,10 +4,15 @@ import {
   Box,
   Button,
   Checkbox,
+  FormControl,
   FormControlLabel,
   Drawer,
   Divider,
+  InputLabel,
+  MenuItem,
   Typography,
+  Select,
+  SelectChangeEvent,
 } from '@mui/material';
 
 import * as React from 'react';
@@ -17,9 +22,35 @@ type Anchor = 'left';
 export const SideBar: React.FC = () => {
   const [container, setContainer] = React.useState([true, false]);
   const [type, setType] = React.useState([true, false]);
-
+  const [hoursAgo, setHoursAgo] = React.useState('');
+  const [upUntil, setUpUntil] = React.useState('');
   const [toggle, setToggle] = React.useState({ left: false });
 
+  /************ Code for time filters in the sidebar ************/
+  // Function for setting hoursAgo state for time filter
+  const hoursFrom = (e: SelectChangeEvent) => {
+    setHoursAgo(e.target.value as string);
+  };
+
+  // Function for setting upUntil state for time filter
+  const hoursTo = (e: SelectChangeEvent) => {
+    setUpUntil(e.target.value as string);
+  };
+
+  // Function for generating hours list for time filter drop down  menus
+  const generateHours = (): JSX.Element[] => {
+    const hours: JSX.Element[] = [];
+    for (let i = 1; i <= 72; i++) {
+      hours.push(<MenuItem value={i}>{i}</MenuItem>);
+    }
+    return hours;
+  };
+
+  // Declaring variable to use for time menu
+  const hours: JSX.Element[] = generateHours();
+  /************ ************/
+
+  // Function for opening and closing the sidebar
   const toggleDrawer =
     (anchor: Anchor, open: boolean) =>
     (event: React.KeyboardEvent | React.MouseEvent) => {
@@ -50,6 +81,7 @@ export const SideBar: React.FC = () => {
   const checkContainer2 = (event: React.ChangeEvent<HTMLInputElement>) => {
     setContainer([container[0], event.target.checked]);
   };
+  /********* **********/
 
   /******** Type filter code ************/
   // Function for checking all type boxes
@@ -64,6 +96,7 @@ export const SideBar: React.FC = () => {
   const checkStderr = (event: React.ChangeEvent<HTMLInputElement>) => {
     setType([type[0], event.target.checked]);
   };
+  /********* **********/
 
   return (
     <>
@@ -129,6 +162,30 @@ export const SideBar: React.FC = () => {
             </Box>
           </List>
           <Divider />
+          <FormControl fullWidth>
+            <InputLabel id="hoursago">Hours Ago</InputLabel>
+            <Select
+              labelId="hoursAgoLabel"
+              id="hoursAgoId"
+              value={hoursAgo}
+              label="HoursAgo"
+              onChange={hoursFrom}
+            >
+              {hours}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id="hoursto">Hours To</InputLabel>
+            <Select
+              labelId="hoursToLabel"
+              id="hoursToId"
+              value={upUntil}
+              label="HoursTo"
+              onChange={hoursTo}
+            >
+              {hours}
+            </Select>
+          </FormControl>
         </Box>
       </Drawer>
     </>
