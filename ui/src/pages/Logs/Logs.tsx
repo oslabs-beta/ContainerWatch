7ca80ae8922cacd2a25b1768441ef17d5429926c
davidchuang5 <davidchuang5@gmail.com>
2023-06-21 19:44:35 -0500

    Refactored useEffect, created state for containers

--- ui/src/pages/Logs/Logs.tsx
+++ ui/src/pages/Logs/Logs.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { createDockerDesktopClient } from '@docker/extension-api-client';
 import { Search, Clear, FilterList, KeyboardArrowUp, KeyboardArrowDown } from '@mui/icons-material';
 import {
   Box,
@@ -19,24 +20,44 @@ import {
 } from '@mui/material';
 import ContainerIcon from '../../components/ContainerIcon/ContainerIcon';
 import SideBar from '../../components/Sidebar/Sidebar';
-import fetchAllContainerLogs, { DockerLog } from '../../actions/fetchAllContainerLogs';
+import fetchAllContainers from '../../actions/fetchAllContainers';
+import fetchAllContainerLogs from '../../actions/fetchAllContainerLogs';
+import { DockerLog, DockerContainer } from '../../types';
 
 const HEADERS = ['', 'Timestamp', 'Container', 'Message'];
+
+// Obtain Docker Desktop client
+const client = createDockerDesktopClient();
+const useDockerDesktopClient = () => {
+  return client;
+};
 
 export default function Logs() {
   // Access the custom theme (provided by DockerMuiThemeProvider)
   const theme = useTheme();
 
+  const ddClient = useDockerDesktopClient();
+
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [containers, setContainers] = useState<DockerContainer[]>([]);
   const [logs, setLogs] = useState<DockerLog[]>([]);
   const [filteredLogs, setFilteredLogs] = useState<DockerLog[]>(logs);
   const [searchText, setSearchText] = useState('');
 
+  console.log(containers);
+
   useEffect(() => {
-    fetchAllContainerLogs().then((allLogs) => {
-      setLogs(allLogs);
-      setFilteredLogs(allLogs);
-    });
+    (async () => {
+      try {
+        const allContainers = await fetchAllContainers(ddClient);
+        const allContainerLogs = await fetchAllContainerLogs(ddClient, allContainers);
+        setContainers(allContainers);
+        setLogs(allContainerLogs);
+        setFilteredLogs(allContainerLogs);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
   }, []);
 
   return (
