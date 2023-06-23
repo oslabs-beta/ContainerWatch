8938bafd257614c6d033f0e67504b49c4b08b77c
Scott Hallock <scott.hallock@gmail.com>
2023-06-23 15:45:35 -0500

    use vite-plugin-svgr

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
+  plugins: [react(), svgr()],
+  base: './',
   build: {
-    outDir: "build",
+    outDir: 'build',
   },
   server: {
     port: 3000,
     strictPort: true,
-  }
+  },
 });
