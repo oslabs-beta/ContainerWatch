5e58e20bb2257f8c182d17fc3afc5253778c7d57
Patrick Vuong <patrickv77@gmail.com>
2023-06-26 15:24:27 -0500

    moved functionality of panel creator into separate action file

---
+++ backend/actions/buildPanel.ts
@@ -0,0 +1,96 @@
+
+
+export default function panelCreator(containerName: string, id: number, promDatasource: any) {
+  if(id === 3) id = 4;
+
+  const targets = [
+    {
+      datasource: promDatasource,
+      editorMode: 'builder',
+      expr: `sum(rate(container_cpu_usage_seconds_total{name=\"${containerName}\"}[$__interval])) * 100`,
+      instant: false,
+      range: true,
+      refId: 'A',
+    },
+  ];
+
+  const dashboardPanel = {
+    datasource: promDatasource,
+    fieldConfig: {
+      defaults: {
+        color: {
+          mode: 'palette-classic',
+        },
+        custom: {
+          axisCenteredZero: false,
+          axisColorMode: 'text',
+          axisLabel: '',
+          axisPlacement: 'auto',
+          barAlignment: 0,
+          drawStyle: 'line',
+          fillOpacity: 0,
+          gradientMode: 'none',
+          hideFrom: {
+            legend: false,
+            tooltip: false,
+            viz: false,
+          },
+          lineInterpolation: 'linear',
+          lineWidth: 1,
+          pointSize: 5,
+          scaleDistribution: {
+            type: 'linear',
+          },
+          showPoints: 'auto',
+          spanNulls: false,
+          stacking: {
+            group: 'A',
+            mode: 'none',
+          },
+          thresholdsStyle: {
+            mode: 'off',
+          },
+        },
+        mappings: [],
+        thresholds: {
+          mode: 'absolute',
+          steps: [
+            {
+              color: 'green',
+              value: null,
+            },
+            {
+              color: 'red',
+              value: 80,
+            },
+          ],
+        },
+      },
+      overrides: [],
+    },
+    gridPos: {
+      h: 8,
+      w: 12,
+      x: 0,
+      y: 0,
+    },
+    options: {
+      legend: {
+        calcs: [],
+        displayMode: 'list',
+        placement: 'bottom',
+        showLegend: true,
+      },
+      tooltip: {
+        mode: 'single',
+        sort: 'none',
+      },
+    },
+    id: id,
+    targets: targets,
+    title: `FROM FRONTEND ${containerName}`,
+    type: 'timeseries',
+  };
+
+  return dashboardPanel;
+};
