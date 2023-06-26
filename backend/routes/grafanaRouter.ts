ba3dd9a1927c7c22695395a7a50e2ad8625055a4
Patrick Vuong <patrickv77@gmail.com>
2023-06-25 23:42:56 -0500

    add router to handle grafana api requests

---
+++ backend/routes/grafanaRouter.ts
@@ -0,0 +1,16 @@
+/**
+ * @module grafanaAPI Router
+ * @description Routes all requests to grafana API
+ */
+
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+// get datasource info from grafana
+router.get()
+
+// POST dashboard to grafana
+router.post()
+
+export default router
