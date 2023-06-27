deb9c88e42705c1f09538471c373c057d48adf1c
Patrick Vuong <patrickv77@gmail.com>
2023-06-27 08:19:53 -0500

    added a few types to types.ts

---
+++ backend/types.ts
@@ -0,0 +1,30 @@
+export type GrafanaDashboard = {
+  dashboard: {
+    id: null | number;
+    title: string;
+    tags: string[];
+    timezone: string;
+    schemaVersion: number;
+    version: number;
+    refresh: string;
+    panels: any[]; // will replace with panel type
+  };
+  folderId: number;
+  message: string;
+  overwrite: boolean;
+}
+
+export type GrafanaDatasource = {
+  type: string;
+  uid: string;
+}
+
+export type GrafanaPanelTarget = {
+  datasource: GrafanaDatasource;
+  editorMode: string;
+  expr: string;
+  instant: boolean;
+  range: boolean;
+  refId: string;
+}
+
