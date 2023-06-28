7f3be6ba87f941bac5c2b5a1fbf55d6fc9b24509
Patrick Vuong <patrickv77@gmail.com>
2023-06-27 20:45:32 -0500

    add comments and formatting for createGrafanaDashboardObject

--- backend/actions/createGrafanaDashboardObject.ts
+++ backend/actions/createGrafanaDashboardObject.ts
@@ -1,16 +1,12 @@
 import createGrafanaPanelObject from './createGrafanaPanelObject.ts';
 import { GrafanaDashboard, GrafanaDatasource } from '../types';
 
-// export default async function dashboardCreator(containerName: string): Promise<Object> {
-
-// export default const createGrafanaDashboardObject: GrafanaDashboard = function(containerName) {
-  
-// }
-
-export default async function createGrafanaDashboardObject(containerID: string, containerName: string, datasource: GrafanaDatasource): Promise<GrafanaDashboard> {
-
-  console.log('in createGrafanadDashboardObject', datasource);
-
+export default async function createGrafanaDashboardObject(
+  containerID: string,
+  containerName: string,
+  datasource: GrafanaDatasource
+): Promise<GrafanaDashboard> {
+  // create dashboard object boilerplate
   const dashboard: GrafanaDashboard = {
     dashboard: {
       id: null,
@@ -27,6 +23,7 @@ export default async function createGrafanaDashboardObject(containerID: string, 
     overwrite: true,
   };
 
+  // push grafana panels into panels key by invoking createGrafanaPanelObject
   dashboard.dashboard.panels.push(createGrafanaPanelObject(containerName, 1, datasource));
 
   return dashboard;
