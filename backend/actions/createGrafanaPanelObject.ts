271da8534a8fcbeb8a96c12eb1f9b790f3f562a3
Patrick Vuong <patrickv77@gmail.com>
2023-06-27 20:28:50 -0500

    add typing to createGrafanaPanelObject function and modularize object creation

--- backend/actions/createGrafanaPanelObject.ts
+++ backend/actions/createGrafanaPanelObject.ts
@@ -1,95 +1,109 @@
-import { GrafanaDatasource } from '../types';
+import {
+  GrafanaDatasource,
+  GrafanaPanelTargetsKey,
+  GrafanaPanelFieldConfigKey,
+  GrafanaPanel,
+  GrafanaPanelOptionsKey,
+} from '../types';
 
-export default function createGrafanaPanelObject(containerName: string, id: number, promDatasource: GrafanaDatasource) {
-  const dashboardPanel = 'helloworld';
-  // const targets = [
-  //   {
-  //     datasource: promDatasource,
-  //     editorMode: 'builder',
-  //     expr: `sum(rate(container_cpu_usage_seconds_total{name="${containerName}"}[$__interval])) * 100`,
-  //     instant: false,
-  //     range: true,
-  //     refId: 'A',
-  //   },
-  // ];
+export default function createGrafanaPanelObject(
+  containerName: string,
+  id: number,
+  promDatasource: GrafanaDatasource
+): GrafanaPanel {
 
-  // const dashboardPanel = {
-  //   datasource: promDatasource,
-  //   fieldConfig: {
-  //     defaults: {
-  //       color: {
-  //         mode: 'palette-classic',
-  //       },
-  //       custom: {
-  //         axisCenteredZero: false,
-  //         axisColorMode: 'text',
-  //         axisLabel: '',
-  //         axisPlacement: 'auto',
-  //         barAlignment: 0,
-  //         drawStyle: 'line',
-  //         fillOpacity: 0,
-  //         gradientMode: 'none',
-  //         hideFrom: {
-  //           legend: false,
-  //           tooltip: false,
-  //           viz: false,
-  //         },
-  //         lineInterpolation: 'linear',
-  //         lineWidth: 1,
-  //         pointSize: 5,
-  //         scaleDistribution: {
-  //           type: 'linear',
-  //         },
-  //         showPoints: 'auto',
-  //         spanNulls: false,
-  //         stacking: {
-  //           group: 'A',
-  //           mode: 'none',
-  //         },
-  //         thresholdsStyle: {
-  //           mode: 'off',
-  //         },
-  //       },
-  //       mappings: [],
-  //       thresholds: {
-  //         mode: 'absolute',
-  //         steps: [
-  //           {
-  //             color: 'green',
-  //             value: null,
-  //           },
-  //           {
-  //             color: 'red',
-  //             value: 80,
-  //           },
-  //         ],
-  //       },
-  //     },
-  //     overrides: [],
-  //   },
-  //   gridPos: {
-  //     h: 8,
-  //     w: 12,
-  //     x: 0,
-  //     y: 0,
-  //   },
-  //   options: {
-  //     legend: {
-  //       calcs: [],
-  //       displayMode: 'list',
-  //       placement: 'bottom',
-  //       showLegend: true,
-  //     },
-  //     tooltip: {
-  //       mode: 'single',
-  //       sort: 'none',
-  //     },
-  //   },
-  //   id: id,
-  //   targets: targets,
-  //   title: `FROM FRONTEND ${containerName}`,
-  //   type: 'timeseries',
-  // };
+  const targets: GrafanaPanelTargetsKey = [
+    {
+      datasource: promDatasource,
+      editorMode: 'builder',
+      expr: `sum(rate(container_cpu_usage_seconds_total{name="${containerName}"}[$__interval])) * 100`,
+      instant: false,
+      range: true,
+      refId: 'A',
+    },
+  ];
+
+  const fieldConfigObject: GrafanaPanelFieldConfigKey = {
+    defaults: {
+      color: {
+        mode: 'palette-classic',
+      },
+      custom: {
+        axisCenteredZero: false,
+        axisColorMode: 'text',
+        axisLabel: '',
+        axisPlacement: 'auto',
+        barAlignment: 0,
+        drawStyle: 'line',
+        fillOpacity: 0,
+        gradientMode: 'none',
+        hideFrom: {
+          legend: false,
+          tooltip: false,
+          viz: false,
+        },
+        lineInterpolation: 'linear',
+        lineWidth: 1,
+        pointSize: 5,
+        scaleDistribution: {
+          type: 'linear',
+        },
+        showPoints: 'auto',
+        spanNulls: false,
+        stacking: {
+          group: 'A',
+          mode: 'none',
+        },
+        thresholdsStyle: {
+          mode: 'off',
+        },
+      },
+      mappings: [],
+      thresholds: {
+        mode: 'absolute',
+        steps: [
+          {
+            color: 'green',
+            value: null,
+          },
+          {
+            color: 'red',
+            value: 80,
+          },
+        ],
+      },
+    },
+    overrides: [],
+  };
+
+  const optionsObject: GrafanaPanelOptionsKey = {
+    legend: {
+      calcs: [],
+      displayMode: 'list',
+      placement: 'bottom',
+      showLegend: true,
+    },
+    tooltip: {
+      mode: 'single',
+      sort: 'none',
+    },
+  };
+
+  const dashboardPanel: GrafanaPanel = {
+    datasource: promDatasource,
+    fieldConfig: fieldConfigObject,
+    gridPos: {
+      h: 8,
+      w: 12,
+      x: 0,
+      y: 0,
+    },
+    options: optionsObject,
+    id: id,
+    targets: targets,
+    title: `${containerName} CPU`,
+    type: 'timeseries',
+  };
 
   return dashboardPanel;
 }
