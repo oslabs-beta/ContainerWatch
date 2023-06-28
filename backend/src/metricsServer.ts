db6dc5823faaab1a1d2b532e261111d0a76cf501
Scott Hallock <scott.hallock@gmail.com>
2023-06-28 17:44:13 -0500

    fix typo in comment

--- backend/src/metricsServer.ts
+++ backend/src/metricsServer.ts
@@ -3,7 +3,7 @@ import { register } from './promClient';
 
 const metricsServer = express();
 
-// Prometheus is configured to scrape docker.host.internal:3333/metrics
+// Prometheus is configured to scrape host.docker.internal:3333/metrics
 metricsServer.get('/metrics', (req, res) => {
   res.setHeader('Content-Type', register.contentType);
   register.metrics().then((data) => res.status(200).send(data));
