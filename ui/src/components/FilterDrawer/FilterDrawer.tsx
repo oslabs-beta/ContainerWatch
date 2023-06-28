7205c1eac626d5cb9cca3e8962d1e30f2e0d25c3
davidchuang5 <davidchuang5@gmail.com>
2023-06-28 14:48:55 -0500

    Prop drill color states down

--- ui/src/components/FilterDrawer/FilterDrawer.tsx
+++ ui/src/components/FilterDrawer/FilterDrawer.tsx
@@ -22,6 +22,7 @@ type FilterDrawerProps = {
   setDrawerOpen: Function;
   setValidFromTimestamp: React.Dispatch<React.SetStateAction<string>>;
   setValidUntilTimestamp: React.Dispatch<React.SetStateAction<string>>;
+  containerLabelColor: Record<string, string>;
 };
 
 export default function FilterDrawer({
