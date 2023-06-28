261add2e60ae83b04c76077a189793b1ad5d9e5c
Patrick Vuong <patrickv77@gmail.com>
2023-06-27 20:27:59 -0500

    create types for grafana dashboard panels

--- backend/types.ts
+++ backend/types.ts
@@ -12,22 +12,96 @@ export type GrafanaDashboard = {
   folderId: number;
   message: string;
   overwrite: boolean;
-}
+};
 
 export type GrafanaDatasource = {
   type: string;
   uid: string;
-}
+};
 
-export type GrafanaPanelTarget = {
-  datasource: GrafanaDatasource;
-  editorMode: string;
-  expr: string;
-  instant: boolean;
-  range: boolean;
-  refId: string;
-}
+export type GrafanaPanelTargetsKey = [
+  {
+    datasource: GrafanaDatasource;
+    editorMode: string;
+    expr: string;
+    instant: boolean;
+    range: boolean;
+    refId: string;
+  }
+];
+
+export type GrafanaPanelFieldConfigKey = {
+  defaults: {
+    color: {
+      mode: string;
+    };
+    custom: {
+      axisCenteredZero: boolean;
+      axisColorMode: string;
+      axisLabel: string;
+      axisPlacement: string;
+      barAlignment: number;
+      drawStyle: string;
+      fillOpacity: number;
+      gradientMode: string;
+      hideFrom: {
+        legend: boolean;
+        tooltip: boolean;
+        viz: boolean;
+      };
+      lineInterpolation: string;
+      lineWidth: number;
+      pointSize: number;
+      scaleDistribution: {
+        type: string;
+      };
+      showPoints: string;
+      spanNulls: boolean;
+      stacking: {
+        group: string;
+        mode: string;
+      };
+      thresholdsStyle: {
+        mode: string;
+      };
+    };
+    mappings: string[];
+    thresholds: {
+      mode: string;
+      steps: {
+        color: string;
+        value: number | null;
+      }[];
+    };
+  };
+  overrides: string[];
+};
+
+export type GrafanaPanelOptionsKey = {
+  legend: {
+    calcs: string[];
+    displayMode: string;
+    placement: string;
+    showLegend: boolean;
+  };
+  tooltip: {
+    mode: string;
+    sort: string;
+  };
+};
 
 export type GrafanaPanel = {
-  
-}
+  datasource: GrafanaDatasource;
+  fieldConfig: GrafanaPanelFieldConfigKey;
+  gridPos: {
+    h: number;
+    w: number;
+    x: number;
+    y: number;
+  };
+  options: GrafanaPanelOptionsKey;
+  id: number;
+  targets: GrafanaPanelTargetsKey;
+  title: string;
+  type: string;
+};
