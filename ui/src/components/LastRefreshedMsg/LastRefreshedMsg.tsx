9dc0b7e059799477f74161c5c2e1f2ad9dec91a0
davidchuang5 <davidchuang5@gmail.com>
2023-06-29 18:08:29 -0500

    Rename component folder and file

---
+++ ui/src/components/LastRefreshedMsg/LastRefreshedMsg.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import { Stack, Typography } from '@mui/material';
+
+type RefreshMessageProps = {
+  elapsedTimeInMinutes: number;
+  setElapsedTimeInMinutes: React.Dispatch<React.SetStateAction<number>>;
+};
+export default function RefreshMessage({
+  elapsedTimeInMinutes,
+  setElapsedTimeInMinutes,
+}: RefreshMessageProps) {
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setElapsedTimeInMinutes((prevElapsedTime) => prevElapsedTime + 1);
+    }, 60000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  let message;
+  if (elapsedTimeInMinutes < 60) {
+    message = `Last refreshed ${elapsedTimeInMinutes}m ago`;
+  } else {
+    const elapsedHours = Math.floor(elapsedTimeInMinutes / 60);
+    const remainingMinutes = elapsedTimeInMinutes % 60;
+    const formattedTime = `${elapsedHours}h ${remainingMinutes}m`;
+    message = `Last refreshed ${formattedTime} ago`;
+  }
+
+  return (
+    <Stack justifyContent={'center'}>
+      <Typography variant="caption">{message}</Typography>
+    </Stack>
+  );
+}
