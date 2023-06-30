4d2ea2edfbaf71189bdc43a6baa904717705d6d7
Patrick Vuong <patrickv77@gmail.com>
2023-06-30 12:22:04 -0500

    add comment for potential stretch goal in index and error message in console for bad post request to grafana api

--- backend/src/actions/startStreamAndCreateDashboard.ts
+++ backend/src/actions/startStreamAndCreateDashboard.ts
@@ -10,20 +10,28 @@ export default async function startStreamAndCreateDashboard(
   containerName: string,
   datasource: GrafanaDatasource
 ) {
-  startContainerMetricsStream(id);
-  const dashboard = createGrafanaDashboardObject(id, containerName, datasource);
+  try {
+    startContainerMetricsStream(id);
+    const dashboard = createGrafanaDashboardObject(id, containerName, datasource);
 
-  // Post request to Grafana API to create a dashboard using the returned dashboard object.
-  await axios.post(
-    'http://host.docker.internal:2999/api/dashboards/db',
-    JSON.stringify(dashboard),
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    // Post request to Grafana API to create a dashboard using the returned dashboard object.
+    const dashboardResponse = await axios.post(
+      'http://host.docker.internal:2999/api/dashboards/db',
+      JSON.stringify(dashboard),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    if(dashboardResponse.status >= 400) {
+      console.log('Error with POST request to Grafana Dashboards API. In createGrafanaDashboardObject.')
     }
-  );
-  
-  // A simple console log to show when graphs are done being posted to Grafana.
-  console.log(`📊 Grafana graphs 📊 for the ${containerName} container are ready!!`);
+
+    // A simple console log to show when graphs are done being posted to Grafana.
+    console.log(`📊 Grafana graphs 📊 for the ${containerName} container are ready!!`);
+  } catch (err) {
+    console.log(err);
+  }
 }
