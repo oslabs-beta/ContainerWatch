f177dd6cfaf2eb8bdd416f1c37191ceda1e305cb
Patrick Vuong <patrickv77@gmail.com>
2023-06-30 17:07:15 -0500

    skeleton for metrics handler in the backend
    Co-authored-by: David Chuang <www.github.com/davidchuang5>

--- backend/src/extensionServer.ts
+++ backend/src/extensionServer.ts
@@ -7,4 +7,30 @@ app.get('/hello', (req, res) => {
   res.send('hello world from the server');
 });
 
+app.post('/api/promQL',
+  getCPUMetrics.metricsController,
+  getMEMMetrics.metricsController,
+  (req, res) => {
+    return res.status(200).json(res.locals.idk);
+  }
+)
+
 export default app;
+
+/*
+
+BODY: {
+  containerID: 'string'
+  time: Date.now() format <-- unix time
+}
+
+try {
+  const response = (await ddClient.extension.vm?.service?.post(
+    '/api/promQL',
+    { BODY }
+  ))
+} catch (err) {
+  console.log(err)
+}
+
+*/
