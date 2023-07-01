e28e93b16254ab293e27facdf676a3667cd1ae9a
Patrick Vuong <patrickv77@gmail.com>
2023-07-01 09:33:22 -0500

    promql query at timestamp progress
    Co-authored-by: David Chuang <www.github.com/davidchuang5>

--- backend/src/controllers/metricsController.ts
+++ backend/src/controllers/metricsController.ts
@@ -1,50 +1,43 @@
-const metricsController = {};
+import axios from "axios";
+import { Request, Response, NextFunction } from "express";
 
-metricsController.getCPUMetrics = async (req, res, next) => {
+const metricsController = function() {};
+
+metricsController.getCPUMetrics = async (req: Request, res: Response, next: NextFunction) => {
   try{
     const { containerID, time } = req.body;
 
     if(!containerID || !time) {
-      //return error
+      // return error
     }
+
+    const CPU_QUERY_STRING = `avg(memory_usage_percent{${containerID}})&time=${time}`
+    const PROMQL_URL = `http://host.docker.internal:9090/api/v1/query?query=${CPU_QUERY_STRING}`
+
+    const prometheusResponse = await axios.get(PROMQL_URL);
+
 
     res.locals.metrics = {};
-    res.locals.metrics['CPU'] = null // PROMQL FETCH HERE
+    res.locals.metrics['CPU'] = prometheusResponse.data.result
 
+    return next();
   } catch (err) {
-
+    console.error(err);
   }
 }
 
-
-
-metricsController.getMEMMetrics = async (res, req, next) => {
-  
+metricsController.getMEMMetrics = async (res: Response, req: Request, next: NextFunction) => {
+   
   try{
     const { containerID, time } = req.body;
     if(!containerID || !time){
-      
+      //return error
     }
-    
+    res.locals.metrics = {};
+    res.locals.metrics['MEM'] = null // PROMQL FETCH HERE
   }
   catch(err){
-
-  }
-}
-
-
-metricsController.getNETIOMetrics = async (res, req, next) => {
-  
-  try{
-    const { containerID, time } = req.body;
-    if(!containerID || !time){
-      
-    }
-    
-    res.locals.metrics['NETIO'] = null // PROMQL FETCH HERE
-  }
-  catch(err){
-
+    console.error(err);
   }
 }
 
@@ -55,3 +48,22 @@ metricsController.getNETIOMetrics = async (res, req, next) => {
 // }
 
 export default metricsController;
+
+
+    /*
+    {
+      status: string,
+      data: {
+        resultType: string,
+        result: [
+          {
+            metric: {},
+            value: [
+              UNIX_TIMESTAMP,
+              VALUE
+            ]
+          }
+        ]
+      }
+    }
+    */
