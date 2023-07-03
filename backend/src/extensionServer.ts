ab96f05b7a0df9774a33310ef415edf7b8987cbc
Scott Hallock <scott.hallock@gmail.com>
2023-07-03 14:44:07 -0500

    wip: alerts backend

--- backend/src/extensionServer.ts
+++ backend/src/extensionServer.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import metricsController from './controllers/metricsController';
+import { UserAlert } from './types';
 
 const app = express();
+
+export const userAlerts: UserAlert[] = [];
 
 // Body parsers
 app.use(express.json());
@@ -23,5 +26,49 @@ app.get(
     return res.status(200).json(res.locals.metrics);
   }
 );
+
+app.get('/api/alerts', (req, res) => {
+  console.log('GET user alerts');
+  console.log(userAlerts);
+  res.json(userAlerts);
+});
+
+app.post('/api/alerts', (req, res) => {
+  const { name, containerId, targetMetric, threshold, email } = req.body;
+  // Need some input validation
+
+  if (name === 'bad') {
+    console.log("User made an alert named 'bad'");
+    return res.status(400).send('Bad input');
+  }
+
+  const newAlert: UserAlert = {
+    uuid: 'asdf', // need uuid here
+    name,
+    containerId,
+    targetMetric,
+    threshold,
+    email,
+    lastExceeded: NaN,
+    lastNotification: NaN,
+    created: Date.now(),
+  };
+
+  userAlerts.push(newAlert);
+  console.log(userAlerts);
+
+  console.log('🚨 User made new alert!');
+  console.log(newAlert);
+
+  res.status(201).json(newAlert);
+});
+
+app.patch('/api/alerts', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete('/api/alerts', (req, res) => {
+  res.sendStatus(200);
+});
 
 export default app;
