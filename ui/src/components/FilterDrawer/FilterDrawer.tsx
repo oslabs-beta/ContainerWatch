a87bc7b1530291729117cdd9bbf2cbdb53bfac11
Jun <junealee07@gmail.com>
2023-07-03 16:17:57 -0400

    delete unused imports

--- ui/src/components/FilterDrawer/FilterDrawer.tsx
+++ ui/src/components/FilterDrawer/FilterDrawer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Launch } from '@mui/icons-material';
 import { debounce } from 'lodash';
 import {
