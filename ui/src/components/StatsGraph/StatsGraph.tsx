af51786e1b151d1e232a115393a2e8fba185ce53
Jun <junealee07@gmail.com>
2023-07-05 10:31:21 -0700

    change select values to include hour and day

--- ui/src/components/StatsGraph/StatsGraph.tsx
+++ ui/src/components/StatsGraph/StatsGraph.tsx
@@ -19,22 +19,29 @@ export default function StatsGraph({ containerName, containerID }: StatsGraphPro
   return (
     <Stack direction="column" spacing={1}>
       <FormControl fullWidth>
-        <InputLabel>Time Frame</InputLabel>
+        <InputLabel>Time</InputLabel>
         <Select label="Age" onChange={handleChange}>
-          <MenuItem value={'15m'}>15 Minutes</MenuItem>
-          <MenuItem value={'60m'}>One Hour</MenuItem>
-          <MenuItem value={'120m'}>Two Hour</MenuItem>
+          <MenuItem value={'5m'}>5 Minutes</MenuItem>
+          <MenuItem value={'15m'}>15 Minute</MenuItem>
+          <MenuItem value={'1h'}>1 Hour</MenuItem>
+          <MenuItem value={'2h'}>2 Hour</MenuItem>
+          <MenuItem value={'1d'}>1 Day</MenuItem>
+          <MenuItem value={'2d'}>2 Day</MenuItem>
+          <MenuItem value={'3d'}>3 Day</MenuItem>
         </Select>
       </FormControl>
       <iframe
-        key={timeFrame}
+        // key={timeFrame}
+        key={`iframe1-${timeFrame}`}
         src={`http://localhost:2999/d-solo/${shortContainerID}/${containerName}?orgId=1&refresh=15s&panelId=1&from=now-${timeFrame}&to=now`}
         width="100%"
         height="200px"
         style={{ border: 0 }}
       />
       <iframe
-        src={`http://localhost:2999/d-solo/${shortContainerID}/${containerName}?orgId=1&refresh=15s&panelId=2&from=now-15m&to=now`}
+        // key={timeFrame}
+        key={`iframe2-${timeFrame}`}
+        src={`http://localhost:2999/d-solo/${shortContainerID}/${containerName}?orgId=1&refresh=15s&panelId=2&from=now-${timeFrame}&to=now`}
         width="100%"
         height="200px"
         style={{ border: 0 }}
