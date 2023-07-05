46bf7b34bd5aed9038e3814e0b05486a0476421f
Jun <junealee07@gmail.com>
2023-07-05 09:43:26 -0700

    able to change time frame for cpu graph

--- ui/src/components/StatsGraph/StatsGraph.tsx
+++ ui/src/components/StatsGraph/StatsGraph.tsx
@@ -11,7 +11,7 @@ export default function StatsGraph({ containerName, containerID }: StatsGraphPro
   // because the full container (64 digits) is too long. Grafana API sets a
   // 40 character limit on a dashboard name.
   const shortContainerID = containerID.slice(0, 12);
-  const [timeFrame, setTimeFrame] = useState('');
+  const [timeFrame, setTimeFrame] = useState('15m');
   const handleChange = (event: any) => {
     setTimeFrame(event.target.value);
   };
@@ -21,13 +21,14 @@ export default function StatsGraph({ containerName, containerID }: StatsGraphPro
       <FormControl fullWidth>
         <InputLabel>Time Frame</InputLabel>
         <Select label="Age" onChange={handleChange}>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          <MenuItem value={'15m'}>15 Minutes</MenuItem>
+          <MenuItem value={'60m'}>One Hour</MenuItem>
+          <MenuItem value={'120m'}>Two Hour</MenuItem>
         </Select>
       </FormControl>
       <iframe
-        src={`http://localhost:2999/d-solo/${shortContainerID}/${containerName}?orgId=1&refresh=15s&panelId=1&from=now-{placeholder}m&to=now`}
+        key={timeFrame}
+        src={`http://localhost:2999/d-solo/${shortContainerID}/${containerName}?orgId=1&refresh=15s&panelId=1&from=now-${timeFrame}&to=now`}
         width="100%"
         height="200px"
         style={{ border: 0 }}
