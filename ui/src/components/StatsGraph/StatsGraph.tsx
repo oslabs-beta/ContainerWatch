0c53736aec848bad175304e71db3465a15cfd54b
Jun <junealee07@gmail.com>
2023-07-05 13:53:59 -0700

    add TimeFrame type

--- ui/src/components/StatsGraph/StatsGraph.tsx
+++ ui/src/components/StatsGraph/StatsGraph.tsx
@@ -6,21 +6,25 @@ type StatsGraphProps = {
   containerID: string;
 };
 
+type TimeFrame = '5m' | '15m' | '1h' | '2h' | '1d' | '2d' | '3d';
+
 export default function StatsGraph({ containerName, containerID }: StatsGraphProps) {
   // The short-form containerID (12 digits) is used for the Grafana dashboards
   // because the full container (64 digits) is too long. Grafana API sets a
   // 40 character limit on a dashboard name.
   const shortContainerID = containerID.slice(0, 12);
-  const [timeFrame, setTimeFrame] = useState('15m');
-  const handleChange = (event: SelectChangeEvent<unknown>) => {
-    setTimeFrame(event.target.value as string);
-  };
+  const [timeFrame, setTimeFrame] = useState<TimeFrame>('15m');
 
   return (
     <Stack direction="column" spacing={1}>
       <FormControl sx={{ m: 0, maxWidth: 130 }} size="small">
         <InputLabel>Time</InputLabel>
-        <Select autoWidth value={timeFrame} label="Age" onChange={(event) => handleChange(event)}>
+        <Select
+          autoWidth
+          value={timeFrame}
+          label="Age"
+          onChange={(e: SelectChangeEvent<TimeFrame>) => setTimeFrame(e.target.value as TimeFrame)}
+        >
           <MenuItem value={'5m'}>5 Minute</MenuItem>
           <MenuItem value={'15m'}>15 Minute</MenuItem>
           <MenuItem value={'1h'}>1 Hour</MenuItem>
