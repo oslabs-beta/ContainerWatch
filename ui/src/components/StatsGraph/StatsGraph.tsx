3f6a9a71a35f3a12f3b2bf4859b6189d064f6526
Patrick Vuong <patrickv77@gmail.com>
2023-07-05 11:51:13 -0500

    only render one statgraph, change timerange to 5m temporarily (to better see new data)

--- ui/src/components/StatsGraph/StatsGraph.tsx
+++ ui/src/components/StatsGraph/StatsGraph.tsx
@@ -14,13 +14,7 @@ export default function StatsGraph({ containerName, containerID }: StatsGraphPro
   return (
     <Stack direction="column" spacing={1}>
       <iframe
-        src={`http://localhost:2999/d-solo/${shortContainerID}/${containerName}?orgId=1&refresh=15s&panelId=1&from=now-15m&to=now`}
-        width="100%"
-        height="200px"
-        style={{ border: 0 }}
-      />
-      <iframe
-        src={`http://localhost:2999/d-solo/${shortContainerID}/${containerName}?orgId=1&refresh=15s&panelId=2&from=now-15m&to=now`}
+        src={`http://localhost:2999/d-solo/${shortContainerID}/${containerName}?orgId=1&refresh=15s&panelId=1&from=now-5m&to=now`}
         width="100%"
         height="200px"
         style={{ border: 0 }}
